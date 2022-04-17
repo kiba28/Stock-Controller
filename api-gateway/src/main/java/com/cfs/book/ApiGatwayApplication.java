fbe26da1d8ef4ca380d894ea707a1b863d75a26e
fsclaudio <42348316+fsclaudio@users.noreply.github.com>
2022-04-16 22:51:59 -0300

    Update ApiGatwayApplication.java

--- api-gateway/src/main/java/com/cfs/book/ApiGatwayApplication.java
+++ api-gateway/src/main/java/com/cfs/book/ApiGatwayApplication.java
@@ -3,7 +3,7 @@ package com.cfs.book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
+//editei
 @SpringBootApplication
 @EnableEurekaClient
 public class ApiGatwayApplication {
